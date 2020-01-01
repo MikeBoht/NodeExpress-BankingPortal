a9b2e98075f8f8626313efb8520321bb433da29b
Mike Boht <mikeboht@gmail.com>
2020-01-01 16:00:27 -0500

    Module 3 complete

--- src/app.js
+++ src/app.js
@@ -9,10 +9,12 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-const accountData = fs.readFileSync(path.join(__dirname, "/json/accounts.json"), {encoding: "UTF8"});
+app.use(express.urlencoded({ extended: true }));
+
+const accountData = fs.readFileSync(path.join(__dirname, "/json/accounts.json"), "utf8");
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, "/json/users.json"), {encoding: "UTF8"});
+const userData = fs.readFileSync(path.join(__dirname, "/json/users.json"), "utf8");
 const users = JSON.parse(userData);
 
 app.get('/', function (req, res) {
@@ -33,6 +35,38 @@ app.get('/credit', function (req, res) {
 
 app.get('/profile', function (req, res) {
     res.render("profile", {user: users[0]});
+});
+
+app.get('/transfer', function (req, res) {
+    res.render("transfer");
+});
+
+app.post('/transfer', function (req, res) {
+    const fromAcc = req.body.from;
+    const toAcc = req.body.to;
+    const amount = parseInt(req.body.amount);
+    accounts[fromAcc].balance = parseInt(accounts[fromAcc].balance) - amount;
+    accounts[toAcc].balance = parseInt(accounts[toAcc].balance) + amount;
+    
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "utf8");
+    
+    res.render("transfer", {message: "Transfer Completed"});
+});
+
+app.get('/payment', function (req, res) {
+    res.render("payment", {account: accounts.credit});
+});
+
+app.post('/payment', function (req, res) {
+    const amount = parseInt(req.body.amount);
+    accounts.credit.balance = parseInt(accounts.credit.balance) - amount;
+    accounts.credit.available = parseInt(accounts.credit.available) + amount;
+    
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, "json/accounts.json"), accountsJSON, "utf8");
+    
+    res.render("payment", {message: "Payment Successful", account: accounts.credit});
 });
 
 app.listen(3000, () => {
